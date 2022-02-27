d700f6efa4e04125ebba4d8e6402e5ddca60ad59
MeftunH <meftun.hesimli@epigra.com>
2022-02-28 01:03:48 +0300

    fix product entity

--- CommentControlSystem/src/main/java/com/CommentControlSystem/Product/entity/Product.java
+++ CommentControlSystem/src/main/java/com/CommentControlSystem/Product/entity/Product.java
@@ -1,12 +1,12 @@
 package com.CommentControlSystem.Product.entity;
 
 import com.CommentControlSystem.General.entity.BaseEntity;
+import com.CommentControlSystem.General.enums.StatusType;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "PRODUCT")
@@ -15,16 +15,24 @@ import javax.persistence.Table;
 public class Product extends BaseEntity {
 
     @Id
+    @SequenceGenerator(name = "Product", sequenceName = "PRODUCT_ID_SEQ", allocationSize = 1)
+    @GeneratedValue(generator = "Product")
     private Long id;
+
+    @Column(name = "NAME", nullable = false)
     private String name;
+
+    @Column(name = "DESCRIPTION", precision = 19, scale = 2)
     private String description;
-    private String category;
-    private String image;
-    private String price;
-    private String quantity;
-    private String status;
-    private String createdBy;
-    private String createdDate;
-    private String updatedBy;
+
+    @Column(name = "PRICE", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "QUANTITY", nullable = false)
+    private int quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "STATUS_TYPE", length = 30)
+    private StatusType statusType;
 
 }
