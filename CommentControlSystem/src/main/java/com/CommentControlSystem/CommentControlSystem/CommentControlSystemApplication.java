18188d86be28374d2b19bf911335b9634d79bd01
MeftunH <meftun.hesimli@epigra.com>
2022-02-28 00:03:35 +0300

    project init

---
+++ CommentControlSystem/src/main/java/com/CommentControlSystem/CommentControlSystem/CommentControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.CommentControlSystem.CommentControlSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentControlSystemApplication.class, args);
+	}
+
+}
---
+++ CommentControlSystem/src/test/java/com/CommentControlSystem/CommentControlSystem/CommentControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CommentControlSystem.CommentControlSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
