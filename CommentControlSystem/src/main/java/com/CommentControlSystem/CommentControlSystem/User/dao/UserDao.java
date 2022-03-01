a713b9831c3c5212be5c2943382c1b15687e1765
MeftunH <meftun.hesimli@epigra.com>
2022-03-01 15:30:45 +0300

    add delete get by nickname and get all users function to user dao

--- CommentControlSystem/src/main/java/com/CommentControlSystem/CommentControlSystem/User/dao/UserDao.java
+++ CommentControlSystem/src/main/java/com/CommentControlSystem/CommentControlSystem/User/dao/UserDao.java
@@ -1,13 +1,15 @@
 package com.CommentControlSystem.CommentControlSystem.User.dao;
 
-import com.CommentControlSystem.CommentControlSystem.Product.entity.Product;
-import com.CommentControlSystem.CommentControlSystem.User.dto.UserDto;
 import com.CommentControlSystem.CommentControlSystem.User.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserDao extends JpaRepository<User, Long> {
     User findByEmail(String email);
     User findByNickname(String nickname);
+    User findByPhoneNumber(String phonebumber);
+    User findByNicknameAndPhoneNumber(String nickname, String phoneNumber);
 }
