1be3acf8496db1c529441db2288659a39ac94525
MeftunH <meftun.hesimli@epigra.com>
2022-03-01 22:58:17 +0300

    add product comment endpoint

--- CommentControlSystem/src/main/java/com/CommentControlSystem/CommentControlSystem/Comment/service/CommentService.java
+++ CommentControlSystem/src/main/java/com/CommentControlSystem/CommentControlSystem/Comment/service/CommentService.java
@@ -47,7 +47,7 @@ public class CommentService {
         List<CommentDto> commentDto = CommentMapper.INSTANCE.convertToCommentDtoList(comments);
         if (commentDto.isEmpty()) {
             User user = userEntityService.getByIdWithControl(id);
-            throw new UserHasNotCommentException(user.getNickname()+"User Has Not Found");
+            throw new UserHasNotCommentException(user.getNickname()+"User Has Not Comment");
         }
         return commentDto;
     }
