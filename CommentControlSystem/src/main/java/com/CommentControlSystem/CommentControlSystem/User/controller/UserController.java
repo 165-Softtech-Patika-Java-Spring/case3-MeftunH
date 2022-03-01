bcdf8a57777be379622bf9b54f2aeb0d532b0055
MeftunH <meftun.hesimli@epigra.com>
2022-03-01 15:30:36 +0300

    add delete get by nickname and get all users function to user controller

--- CommentControlSystem/src/main/java/com/CommentControlSystem/CommentControlSystem/User/controller/UserController.java
+++ CommentControlSystem/src/main/java/com/CommentControlSystem/CommentControlSystem/User/controller/UserController.java
@@ -3,19 +3,17 @@ package com.CommentControlSystem.CommentControlSystem.User.controller;
 import com.CommentControlSystem.CommentControlSystem.General.dto.RestResponse;
 import com.CommentControlSystem.CommentControlSystem.User.dto.UserDto;
 import com.CommentControlSystem.CommentControlSystem.User.dto.UserSaveRequestDto;
+import com.CommentControlSystem.CommentControlSystem.User.dto.UserUpdateRequestDto;
 import com.CommentControlSystem.CommentControlSystem.User.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -37,9 +35,48 @@ public class UserController {
 
     @PostMapping
     @Valid
-    public ResponseEntity createProduct(@RequestBody UserSaveRequestDto userDTO) {
+    public ResponseEntity createProduct(@RequestBody @Valid UserSaveRequestDto userDTO) {
         UserDto userDto = userService.save(userDTO);
         return ResponseEntity
                 .ok(RestResponse.of(userDto));
+    }
+
+    @GetMapping
+    public ResponseEntity getAllUsers() {
+        List<UserDto> userDto = userService.findAll();
+        return ResponseEntity
+                .ok(RestResponse.of(userDto));
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity getUserByNickname(@PathVariable String username) {
+
+        UserDto userDto = userService.findByNickname(username);
+        return ResponseEntity
+                .ok(RestResponse.of(userDto));
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity getUserById(@PathVariable Long id) {
+
+        UserDto userDto = userService.findById(id);
+        return ResponseEntity
+                .ok(RestResponse.of(userDto));
+    }
+
+    @PutMapping
+    public ResponseEntity update(@RequestBody UserUpdateRequestDto userDTO) {
+
+        UserDto userDto = userService.update(userDTO);
+
+        return ResponseEntity.ok(RestResponse.of(userDto));
+    }
+
+    @DeleteMapping("/{nickname}/{phonenumber}")
+    public ResponseEntity delete(@PathVariable String nickname, @PathVariable String phonenumber) {
+
+        userService.delete(nickname, phonenumber);
+
+        return ResponseEntity.ok(RestResponse.empty());
     }
 }
