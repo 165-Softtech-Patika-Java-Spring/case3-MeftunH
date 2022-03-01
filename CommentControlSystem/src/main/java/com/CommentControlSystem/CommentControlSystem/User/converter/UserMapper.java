dc1c4bb1f1858de1737a1fb4db416b226741a1b9
MeftunH <meftun.hesimli@epigra.com>
2022-03-01 15:33:05 +0300

    add convert to user and convert to user dto list functions to user mapper

--- CommentControlSystem/src/main/java/com/CommentControlSystem/CommentControlSystem/User/converter/UserMapper.java
+++ CommentControlSystem/src/main/java/com/CommentControlSystem/CommentControlSystem/User/converter/UserMapper.java
@@ -1,11 +1,16 @@
 package com.CommentControlSystem.CommentControlSystem.User.converter;
 
+import com.CommentControlSystem.CommentControlSystem.Product.dto.ProductDto;
+import com.CommentControlSystem.CommentControlSystem.Product.entity.Product;
 import com.CommentControlSystem.CommentControlSystem.User.dto.UserDto;
 import com.CommentControlSystem.CommentControlSystem.User.dto.UserSaveRequestDto;
+import com.CommentControlSystem.CommentControlSystem.User.dto.UserUpdateRequestDto;
 import com.CommentControlSystem.CommentControlSystem.User.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
@@ -13,4 +18,7 @@ public interface UserMapper {
 
     UserDto convertToUserDto(User user);
     User convertToUser(UserSaveRequestDto userSaveRequestDto);
+    User convertToUser(UserUpdateRequestDto userUpdateRequestDto);
+    List<UserDto> convertToUserDtoList(List<User> userDto);
+
 }
